175d878c070bb8f4e24733102872c8aec5821676
Franco Battagion <fbattagion@hotmail.com>
2018-07-19 12:37:06 -0300

    Se incorporó la splash screen de ceibo, al inicio.

--- MaminaProject/Views/App.xaml.cs
+++ MaminaProject/Views/App.xaml.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Diagnostics;
+using System.Threading;
+
 
 namespace Views
 {
@@ -13,5 +16,45 @@ namespace Views
     /// </summary>
     public partial class App : Application
     {
+
+        private const int MINIMUM_SPLASH_TIME = 1500; // milisecs
+        private const int SPLASH_FADE_TIME = 500;     //milisecs
+
+
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+
+
+            SplashScreen splash = new SplashScreen("Images/ceibo_splash.png");
+
+
+            splash.Show(false, true); //cargar splash screen
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start(); //empezar stopwatch
+
+
+            base.OnStartup(e);
+            MainWindow main = new MainWindow(); //load/cargar ventana
+
+
+            timer.Stop(); //timer para que dure 2 segundos en pantalla
+            int remainingTimeToShowSplash = MINIMUM_SPLASH_TIME - (int)timer.ElapsedMilliseconds;
+            if (remainingTimeToShowSplash > 0)
+                Thread.Sleep(remainingTimeToShowSplash);
+
+
+            splash.Close(TimeSpan.FromMilliseconds(SPLASH_FADE_TIME)); //show
+            //main.Show();
+
+           
+
+
+
+
+        }
+
     }
 }
